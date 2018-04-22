d5b9c22237b16c711fb86a80979e4b089ce35032
Apache3 <tj.thomas.jeantet@gmail.com>
2018-04-22 21:04:34 +0200

    new pin configuration

--- actuators_master/pins.h
+++ actuators_master/pins.h
@@ -1,19 +1,21 @@
 #include "Arduino.h"
 
-#define SONAR_1_ECHO	3
-#define SONAR_1_TRIG	2
-#define SONAR_2_ECHO	5
-#define SONAR_2_TRIG	4
-#define SONAR_3_ECHO	7
-#define SONAR_3_TRIG	6
-#define SONAR_4_ECHO	9
-#define SONAR_4_TRIG	8
-#define PLIERS_LIMIT_SWITCH_PIN	9 
-#define SPI_SS_PIN	10
-#define EN_PIN		11
-#define DIR_PIN 	12
-#define STEP_PIN 	13
+#define MOTOR_A_IN_1	4
+#define MOTOR_A_IN_2	7
+#define MOTOR_A_EN		5
+#define MOTOR_B_IN_1	8
+#define MOTOR_B_IN_2	9
+#define MOTOR_B_EN		6
+
+#define STEPPER_A_STEP	A1
+#define STEPPER_A_DIR	A0
+#define STEPPER_B_STEP	A3
+#define STEPPER_B_DIR	A2
+
+#define PLIERS_LIMIT_SWITCH_PIN	A4
+#define SPI_SS_PIN	13
+//#define EN_PIN		11
+//#define DIR_PIN 	12
+//#define STEP_PIN 	13
 
 
-#define UP 			true
-#define DOWN 		false
