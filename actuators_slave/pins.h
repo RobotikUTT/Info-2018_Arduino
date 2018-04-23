c0fdcfe4145f60f4c254aeddd4a98d69ea20e54b
Apache3 <tj.thomas.jeantet@gmail.com>
2018-04-23 20:00:15 +0200

    addapted for the real pinout on the robot

--- actuators_slave/pins.h
+++ actuators_slave/pins.h
@@ -1,18 +1,33 @@
 #include "Arduino.h"
 
-#define SONAR_1_ECHO	3
+#define SONAR_0_ECHO	A0
+#define SONAR_0_TRIG	A1
+#define SONAR_1_ECHO	4
 #define SONAR_1_TRIG	2
-#define SONAR_2_ECHO	5
-#define SONAR_2_TRIG	4
-#define SONAR_3_ECHO	7
-#define SONAR_3_TRIG	6
+#define SONAR_2_ECHO	A2
+#define SONAR_2_TRIG	A3
+#define SONAR_3_ECHO	8
+#define SONAR_3_TRIG	7
 #define SONAR_4_ECHO	9
-#define SONAR_4_TRIG	8
-#define PLIERS_LIMIT_SWITCH_PIN	9 
-#define SPI_SS_PIN	10
-#define EN_PIN		11
-#define DIR_PIN 	12
-#define STEP_PIN 	13
+#define SONAR_4_TRIG	10
+#define SONAR_5_ECHO	13
+#define SONAR_5_TRIG	12
+
+#define SEVRVO_0		5
+#define SEVRVO_1		6
+
+#define SONAR_FRONT_L_ECHO	SONAR_0_ECHO
+#define SONAR_FRONT_L_TRIG	SONAR_0_TRIG
+#define SONAR_FRONT_R_ECHO	SONAR_1_ECHO
+#define SONAR_FRONT_R_TRIG	SONAR_1_TRIG
+#define SONAR_L_ECHO		SONAR_2_ECHO
+#define SONAR_L_TRIG		SONAR_2_TRIG
+#define SONAR_R_ECHO		SONAR_3_ECHO
+#define SONAR_R_TRIG		SONAR_3_TRIG
+#define SONAR_BACK_ECHO		SONAR_4_ECHO
+#define SONAR_BACK_TRIG		SONAR_4_TRIG
+// #define SONAR_5_ECHO	13
+// #define SONAR_5_TRIG	12
 
 
 #define UP 			true
