9a67276b9a5826b62092b17b1cb2b057f553d74b
Apache3 <tj.thomas.jeantet@gmail.com>
2018-04-25 13:49:14 +0200

    initial commit

---
+++ libs/MotorControl/MotorControl.h
@@ -0,0 +1,38 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+/** Includes **/
+/**************/
+
+#include "Arduino.h"
+
+/** Defines **/
+/*************/
+
+
+/** Class Descritpion **/
+
+class MotorControl
+{
+	private:
+		uint8_t m_enable_pin;
+		uint8_t m_dir_pin;
+		uint8_t m_brake_pin;
+		uint8_t m_foreward_sense;
+		uint8_t m_backward_sense;
+
+
+
+	public:
+		MotorControl(uint8_t enable_pin, uint8_t dir_pin, uint8_t brake_pin, uint8_t foreward_sense);
+		MotorControl(uint8_t enable_pin, uint8_t dir_pin, uint8_t foreward_sense);
+		void setSpeed(int pwm);
+
+		void stop();
+		void motor_brake();
+
+
+};
+
+
+#endif //MOTOR_CONTROL_H
