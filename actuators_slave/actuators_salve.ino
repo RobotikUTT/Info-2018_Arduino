35e1a86a7dc09e2b82dcf78e2c81950acd863c00
Apache3 <tj.thomas.jeantet@gmail.com>
2018-04-25 13:51:53 +0200

    added com with master

--- actuators_slave/actuators_salve.ino
+++ actuators_slave/actuators_salve.ino
@@ -14,9 +14,13 @@
 // this arduino controls the pliers (a stepper, an AX12 and a limit switch)
 // it also controls a L298 double H-bridge (aka the ball thrower)
 
-#define SERIAL_DELAY	100
+#define SONAR_DELAY	            100
+#define EMERGENCY_SONAR_DELAY   30
 
-SonarArray sonarArray((uint8_t)4,SONAR_FRONT_L_TRIG,SONAR_FRONT_L_ECHO,
+void readMaster();
+void writeMaster(uint8_t* message, uint8_t size);
+
+SonarArray sonarArray((uint8_t)5,SONAR_FRONT_L_TRIG,SONAR_FRONT_L_ECHO,
 			 SONAR_FRONT_R_TRIG,SONAR_FRONT_R_ECHO,
 			 SONAR_L_TRIG,SONAR_L_ECHO,
 			 SONAR_R_TRIG,SONAR_R_ECHO,
@@ -24,32 +28,46 @@ SonarArray sonarArray((uint8_t)4,SONAR_FRONT_L_TRIG,SONAR_FRONT_L_ECHO,
 
 void decodeFrame(uint8_t* message);
 
-uint32_t orderMillis = millis();
+uint32_t sonarMillis = millis();
 void setup()
 {
-	Serial.begin(115200);
-	Serial.println("Hello World!");
-
+	Serial.begin(BAUDRATE);
 }
 
 void loop()
 {
 	sonarArray.update();
 
-	if (millis() - orderMillis > SERIAL_DELAY)
+    uint32_t sonar_delay;
+    if( sonarArray.detectTooClose() )
+    {
+        sonar_delay = EMERGENCY_SONAR_DELAY;
+    }
+    else
+    {
+        sonar_delay = SONAR_DELAY;
+    }
+	if (millis() - sonarMillis > sonar_delay)
 	{
-		sonarArray.disableAll();
-		std::vector<uint16_t> sonar_dist = sonarArray.getDistances();
+        uint8_t sonarFrame[MSG_SIZE];
 
-		std::vector<uint16_t>::iterator it;
-		for (it = sonar_dist.begin(); it != sonar_dist.end(); it++)
-		{
-			Serial.print(*it);
-			Serial.print("|");
-		}
-		Serial.println();
-		sonarArray.enableAll();
-		orderMillis = millis();
+        std::vector<uint8_t> sonar_dist = sonarArray.getNormalizedDistances();
+
+        uint8_t size = encodeFrame(sonarFrame, SONAR_DISTANCE, 
+            sonar_dist[0], sonar_dist[1],sonar_dist[2],
+            sonar_dist[3], sonar_dist[4]);
+        sonarArray.disableAll();
+        writeMaster(sonarFrame, size);
+        // std::vector<uint16_t>::iterator it;
+        // for (it = sonar_dist.begin(); it != sonar_dist.end(); it++)
+        // {
+        //     Serial.print(*it);
+        //     Serial.print("|");
+        // }
+        // Serial.println();
+        sonarArray.enableAll();
+	
+		sonarMillis = millis();
 	}
 }
 
@@ -58,34 +76,35 @@ void decodeFrame(uint8_t* message)
     uint8_t mode = message[0];
     switch (mode) 
     {
-        case HANDSHAKE:
-        {           
-            break;
-        }
-
-        case SET_MODE:
+        case SET_SERVO :
         {
-            break;
-        }
-
-        
-
-        case MOVE_PLIERS :
-        {
-            break;
-        }
-
-        case CLOSE_OPEN_PLIERS :
-        {
-            break;
-        }
-
-        case THROW_BALLS :
-        {
+            uint8_t sonarID = message[1];
+            uint8_t angle = message[2];
+            // send order to servo
             break;
         }
         default:
             break;
 
     }
+}
+
+void readMaster()
+{
+    if (Serial.available() > 0)
+    {
+        uint8_t buffer_in[MSG_SIZE];
+        Serial.readBytesUntil('\n',(char*)buffer_in, MSG_SIZE);
+        decodeFrame(buffer_in);
+    }
+}
+
+void writeMaster(uint8_t* message, uint8_t size)
+{
+    for (uint8_t i = 0; i < size; i++)
+    {
+        Serial.write(message[i]);
+    }
+
+    Serial.print('\n');
 }
