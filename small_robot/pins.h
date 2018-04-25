5644b7b7e580ecc1bdf15af34610565c965274d6
romain <tho.romain@live.fr>
2018-04-25 18:57:24 +0200

    ajout_pins

---
+++ small_robot/pins.h
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+
+#define PHOTO_0		A0
+#define PHOTO_1		A1
+#define PHOTO_2		A6
+#define PHOTO_3		A2
+#define PHOTO_4		A3
+
+#define MOTOR_A_EN	6
+#define MOTOR_A_I1	8
+#define MOTOR_A_I2	7
+#define MOTOR_B_EN	5
+#define MOTOR_B_I1	2
+#define MOTOR_B_I2	4
